bf07d1c896022605df8964784a580273788e0525
asifahmed1us1@gmail.com <asifahmed1us1@gmail.com>
2021-01-30 19:50:13 +0500

    js script 2

--- javascript/app.js
+++ javascript/app.js
@@ -19,3 +19,33 @@ console.log(array2[9])
 for(var lop = 0;lop<5;lop++){
     console.log(lop)
 }
+
+
+
+var abc = prompt("Please enter your name")
+// console.log(`this is ${abc}`)
+switch(abc){
+    case "Asif":
+        console.log(`Case 1 this is ${abc}`)
+        break;
+    case "Imran":
+        console.log(`Case 2 this is ${abc}`)
+    default:
+        console.log("this is default")
+}   
+
+
+var btn = document.getElementById("event")
+btn.addEventListener("click",()=>{
+    console.log("event listener")
+    btn.style.backgroundColor = "blue";
+    btn.style.color = "white";
+})
+
+
+
+function readMore(){
+    // alert("readmore")
+    var p = document.getElementById("para")
+    p.innerHTML = "Asif Ahmed is a bad boy and your are he badliest boy every i seen"
+}
