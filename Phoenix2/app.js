7a82856b6af20f9e63bbf3bd24fbf17cf0f267bd
Asif <asifahmed1us1@gmail.com>
2021-02-10 23:01:40 +0500

    started again

---
+++ Phoenix2/app.js
@@ -0,0 +1,9 @@
+let userid = document.getElementById("userid")
+let password = document.getElementById("password")
+
+
+document.getElementById("signin_btn").addEventListener("click",function(){
+    // alert("hello")
+    console.log(userid.value, password.value)
+
+})
