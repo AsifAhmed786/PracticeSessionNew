57627750da87b1f6300f64a061c799d20b38c728
Asif <asifahmed1us1@gmail.com>
2021-03-07 20:10:49 +0500

    js practice

---
+++ Phoenix6/app.js
@@ -0,0 +1,25 @@
+var a = "hello world"
+console.log(a)
+
+var name1 = prompt("Please enter your name")
+console.log(name1)
+
+for(var i = 0;i<4; i++){
+    console.log(i)
+}
+
+
+var count = 0;
+var status = true;
+while(status){
+    count++;
+    console.log(count)
+    if(count==4){
+        break
+    }
+
+}
+
+var prmp = +prompt("Please enter figure multiplied by 5")*5
+console.log(prmp)
+
